c8076979d429f465b51a72814abd40057b168f9c
StephhyL <Stephanie Li>
2022-01-05 14:57:33 -0500

    store categories count into instance variable

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -3,6 +3,6 @@ class Admin::DashboardController < ApplicationController
                                password: ENV['ADMIN_PASSWORD']
   def show
     @total_product_count = Product.all.sum(:quantity)
-    @
+    @total_category_count = Category.all.count
   end
 end
