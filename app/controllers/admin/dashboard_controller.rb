b2bb046dee9b0956b0f425ec218ad69975d5c4eb
StephhyL <Stephanie Li>
2022-01-05 14:52:18 -0500

    query for total product count and stored in insance variable

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -2,6 +2,7 @@ class Admin::DashboardController < ApplicationController
   http_basic_authenticate_with name: ENV['ADMIN_USERNAME'],
                                password: ENV['ADMIN_PASSWORD']
   def show
-    @product_count = Product.all.sum(:quantity)
+    @total_product_count = Product.all.sum(:quantity)
+    @
   end
 end
