51a30f93eb4f6eb477ba3216139b9726697446d1
StephhyL <Stephanie Li>
2022-01-06 09:54:16 -0500

    add private method for category_params

--- app/controllers/admin/categories_controller.rb
+++ app/controllers/admin/categories_controller.rb
@@ -6,7 +6,23 @@ class Admin::CategoriesController < ApplicationController
     @categories = Category.order(:name).all
   end
 
-  def new; end
+  def new
+    @category = Category.new
+  end
 
-  def create; end
+  def create
+    @category = Category.new(category_params)
+
+    if @category.save
+      redirect_to %i[admin categories], notice: 'Category created!'
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def category_params
+    params.require(:category).permit(:name)
+  end
 end
