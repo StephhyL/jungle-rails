6eae0c98432041875be6153ca4b3acab96c81c09
StephhyL <Stephanie Li>
2022-01-06 13:35:17 -0500

    add notice when user created and flash error if one occurred

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,11 +2,12 @@ class UsersController < ApplicationController
   def new; end
 
   def create
-    user = User.new(user_params)
-    if user.save
+    @user = User.new(user_params)
+    if @user.save
       session[:user_id] = user.id
-      redirect_to '/'
+      redirect_to '/', notice: 'User created!'
     else
+      flash[:error] = 'An error occured!'
       redirect_to '/register'
     end
   end
