3d4a14f6f4483b36100e9b36a5e4104ca1d02533
StephhyL <Stephanie Li>
2022-01-06 13:27:51 -0500

    edit create redirect links if there is new user successfully created

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,17 +4,18 @@ class UsersController < ApplicationController
   def create
     user = User.new(user_params)
     if user.save
-      session[:user_id] = user.id 
-      redirect_to /
+      session[:user_id] = user.id
+      redirect_to '/'
     else
-      redirect_to '/signup'
+      redirect_to '/register'
     end
   end
 
-  private 
+  private
 
   def user_params
-    params.require(:user).permit(:name, :email, :password, :password_confirmation)
+    params
+      .require(:user)
+      .permit(:name, :email, :password, :password_confirmation)
   end
-
 end
