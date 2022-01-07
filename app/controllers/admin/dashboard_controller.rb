657a333eabb21f6154faffbe7956a05807a3c910
StephhyL <Stephanie Li>
2022-01-07 09:38:03 -0500

    add instance variables for total_active-sale_count and total_upcoming_sale_count for dashboard show method

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -4,5 +4,16 @@ class Admin::DashboardController < ApplicationController
   def show
     @total_product_count = Product.all.sum(:quantity)
     @total_category_count = Category.all.count
+    @total_active_sale_count =
+      Sale
+        .all
+        .where(
+          'sales.starts_on <= ? AND sales.ends_on >= ?',
+          Date.current,
+          Date.current,
+        )
+        .count
+    @total_upcoming_sale_count =
+      Sale.all.where('sales.starts_on >=?', Date.current).count
   end
 end
