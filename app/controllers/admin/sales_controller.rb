3a77abfdef6a53233356b49730059214b8703f19
StephhyL <Stephanie Li>
2022-01-07 08:43:15 -0500

    add new method

--- app/controllers/admin/sales_controller.rb
+++ app/controllers/admin/sales_controller.rb
@@ -2,12 +2,12 @@ class Admin::SalesController < ApplicationController
   ApplicationController
   http_basic_authenticate_with name: ENV['ADMIN_USERNAME'],
                                password: ENV['ADMIN_PASSWORD']
-  def index
-
-
-
 
   def index
     @sales = Sale.all
+  end
+
+  def new
+    @sales = Sale.new
   end
 end
