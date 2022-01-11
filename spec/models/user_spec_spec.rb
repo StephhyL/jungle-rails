945cb6282d6a1bd3d581611ed10f6391530970dd
StephhyL <Stephanie Li>
2022-01-10 20:01:11 -0500

    validation tests for first_name, last_name, email, password

---
+++ spec/models/user_spec_spec.rb
@@ -0,0 +1,58 @@
+require 'rails_helper'
+
+RSpec.describe User, type: :model do
+  describe 'Validations' do
+    before do
+      @user =
+        User.create(
+          first_name: 'Jen',
+          last_name: 'Kim',
+          email: 'people@sunny.com',
+          password: '12345',
+          password_confirmation: '12345',
+        )
+    end
+
+    it 'valid with all necessary attributes' do
+      expect(@user).to be_valid
+    end
+
+    it 'should have a valid first_name' do
+      @user.first_name = nil
+      @user.valid?
+      expect(@user.errors.full_messages).to_not be_empty
+    end
+
+    it 'should have a valid last_name' do
+      @user.last_name = nil
+      @user.valid?
+      expect(@user.errors.full_messages).to_not be_empty
+    end
+
+    it 'should have an email present' do
+      @user.email = nil
+      @user.valid?
+      expect(@user.errors.full_messages).to_not be_empty
+    end
+
+    it 'should have a unique email' do
+      @user2 =
+        User.create(
+          first_name: 'Mickey',
+          last_name: 'Mouse',
+          email: 'PEOPLE@sunny.com',
+          password: '12345',
+          password_confirmation: '12345',
+        )
+
+      @user2.valid?
+      expect(@user2.errors.full_messages).to_not be_empty
+    end
+
+    it 'should have entered the same thing for password and password confirmation' do
+      @user.password_confirmation = '3243242342'
+      @user.valid?
+      expect(@user.errors.full_messages).to_not be_empty
+    end
+  end
+end
